cc0f4feaaf11c4ac563f7553179428f98d81a9c9
Aryan Singh Thakur <aryansingh102003@gmail.com>
2024-07-30 16:58:10 +0530

    Changes Added

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cors(
     {
         origin: 'https://task-master-crework.vercel.app/',
-        credentials: true
+        methods: ['GET', 'POST', 'PUT', 'DELETE']
     }
 ));
 app.use(bodyParser.json());
--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://task-master-six-nu.vercel.app/',
 });
 
 export default api;
