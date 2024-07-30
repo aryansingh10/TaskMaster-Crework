77d3c7e6d821684b0b38a188ff0aa6692aef8661
Aryan Singh Thakur <aryansingh102003@gmail.com>
2024-07-30 16:36:42 +0530

    Added

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,9 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: 'Something went wrong!' });
 });
+app.get('/', (req, res) => {
+    res.send('Welcome to the Task Manager API');
+});
 
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
